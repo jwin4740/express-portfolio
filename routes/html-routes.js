e8bd03c7aed7f9d20c4042f28a52b759693416f0
James Winkle <jwin4740@gmail.com>
2017-05-20 08:45:44 -0400

    first deploy

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -18,6 +18,18 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
+       app.get("/about", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/about.html"));
+    });
+
+       app.get("/portfolio", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/portfolio.html"));
+    });
+
+       app.get("/resume", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/resume.html"));
+    });
+
 
     app.use(function (req, res, next) {
         res.status(404).send("<div style='text-align: center;'><h1> 404 Error</h1><h3> The page you requested can't be found </h3><h4> return to minervaChess</h4></div>");
